1a3dd84f87d8e343a48c6e8284975f385d74885f
willberRR <rivasrodriguezwillber10@gmail.com>
2021-10-26 13:35:54 +0000

    [ADD] Basic Module

---
+++ odoo_academy/__manifest__.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+
+{ 
+    'name': 'Odoo Academy',
+     
+    ' Summary': """ Academy app to manage Training WillberRR""",
+    
+    'descripton': """
+        Academy Module to manage  Trianing:
+        - Course
+        -Sessions
+        -Attendess
+    """,
+     
+    'autor': 'WillberRR',
+    'version': '0.1  to Odoo 14'
+    
+    'depends': ['base'],
+    
+    'data':[
+        
+    ],
+    
+    'demo': [
+        
+    ],
+    
+}
